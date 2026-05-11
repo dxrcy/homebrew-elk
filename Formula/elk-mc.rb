e68ad0a5a1c83a01c5d2cf21937fa59c2000c255
ida5428 <207427661+ida5428@users.noreply.github.com>
2026-05-11 13:39:28 +1000

    fix: update formulae to match new `version.json` structure

--- Formula/elk-mc.rb
+++ Formula/elk-mc.rb
@@ -21,19 +21,19 @@ class ElkMc < Formula
   on_macos do
     on_arm do
       url "https://github.com/dxrcy/elk/releases/download/v#{version}-mc/elk-macos-arm64"
-      sha256 meta["sha256"]["mc"]["macos-arm64"]
+      sha256 meta["mc"]["sha256"]["macos-arm64"]
     end
 
     on_intel do
       url "https://github.com/dxrcy/elk/releases/download/v#{version}-mc/elk-macos-x64"
-      sha256 meta["sha256"]["mc"]["macos-x64"]
+      sha256 meta["mc"]["sha256"]["macos-x64"]
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/dxrcy/elk/releases/download/v#{version}-mc/elk-linux-x64"
-      sha256 meta["sha256"]["mc"]["linux-x64"]
+      sha256 meta["mc"]["sha256"]["linux-x64"]
     end
   end
 
